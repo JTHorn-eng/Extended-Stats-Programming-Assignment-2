c91e95207097736dd96b42f7e6dc88b3319967bc
Matthew Poole <149334794+mapletoot@users.noreply.github.com>
2025-10-16 09:07:34 +0100

    Cleaned comments

--- Submission.R
+++ Submission.R
@@ -8,49 +8,59 @@
 #
 ### About
 #
-# This document is the code to run an SEIR model and prints the plots of 4 different implementations of the model.
-# The program primarily uses two functions to do this: get.net() and seir().
-# This SEIR model can account for different transmission rates between people sharing houses of different sizes,
-# social networks of different sizes, and random mixing. The housing sharing assumes a uniform distribution of house sizes and
-# the social networks allow for variable social network sizes, depending on their 'sociability' parameter.
+# This document is the code to run an SEIR model and prints the plots of 4 
+# different implementations of the model. The program primarily uses two 
+# functions to do this: get.net() and seir(). This SEIR model can account for 
+# different transmission rates between people sharing houses of different sizes,
+# social networks of different sizes, and random mixing. The housing sharing 
+# assumes a uniform distribution of house sizes and the social networks allow 
+# for variable social network sizes, depending on their 'sociability' parameter.
 # The code concludes with a commentary on the 4 different models. 
 # 
 #
 #
 ### Contributions:
 #
-# All members attended the majority of workshops and communicated remotely about progress made outside of
-# class time.
+# All members attended the majority of workshops and communicated remotely about 
+# progress made outside of class time.
 #
-# Colin McDonagh (33 %) Wrote tha majority of the nseir function and led the commentary of the different models  
+# Colin McDonagh (33 %) Wrote the majority of the nseir function and led the 
+#                       commentary of the different models  
 #                      
-# Matthew Poole  (34 %) Wrote the code to distribute people to houses, optimised the get.net function and nseir function
+# Matthew Poole  (34 %) Wrote the code to distribute people to houses, optimised 
+#                       the get.net function and nseir function
 #                      
-# James Horn     (33 %) Wrote the original get.net function and wrote the plot function 
+# James Horn     (33 %) Wrote the original get.net function and wrote the plot 
+#                       function 
 #                     
 
 
 get.net = function(beta, h, nc=15) {
-  #' Creates a list of vectors, with each vector at position i containing the indices of people that person i is connected with socially.
-  #' This is done by looping through each person, calculating their network based on the probability they are connected to someone else, 
-  #' and then recording these values at position i of the list, and then recording the value i at each index of the list of the person
-  #' in the social network.
-  #' @param beta A list of uniform random deviates
-  #' @param h a vector describing who shares houses
-  #' @param n_c a value dictating the average number of connections per person.
+  #' Creates a list of vectors, with each vector at position i containing the 
+  #' indices of people that person i is connected with socially. This is done by 
+  #' looping through each person, calculating their network based on the 
+  #' probability they are connected to someone else, and then recording these 
+  #' values at position i of the list, and then recording the value i at each 
+  #' index of the list of the person in the social network.
+  #' @param beta A list of n uniform random deviates
+  #' @param h    A vector of length n describing who shares houses
+  #' @param n_c  A value dictating the average number of connections per person.
   #' 
-  #' @return a list of vectors, with the vector at position i being the social network of person i.
+  #' @return a list of vectors, with the vector at position i being the social 
+  #' network of person i.
   
   # Define the number of people to consider
   n <- length(h)
   
-  # Create a list of vectors for who shares a house with who, as people sharing houses cannot be in the same social network.
+  # Create a list of vectors for who shares a house with who, as people sharing 
+  # houses cannot be in the same social network.
   house_network <- lapply(h, function(x) which(x == h))
   
   # Generate an empty list to populate with connections
   social_network <- vector("list",n)
   
-  # Calculates a constant used inside a loop once for efficiency to calculate the probability that two people are connected socially.
+  # Calculates a constant used inside a loop once for efficiency to calculate 
+  # the probability that two people are connected socially.
   k <- nc*beta/(mean(beta)^2*(n-1))
   
   # loop through each person in the network
@@ -60,39 +70,53 @@ get.net = function(beta, h, nc=15) {
     network_probs <- c(rep(0,i), beta[i]*k[(i+1):n]) 
     
     # Create probabilities to compare the network probs with.
-    # The first i values are 1 as the first (i-1) have already been assigned networks, and person i cannot be in a network with themself. 
+    # The first i values are 1 as the first (i-1) have already been assigned 
+    # networks, and person i cannot be in a network with themself. 
     probs <- c(rep(1,i), runif(n-i))
     
-    # Assign the indices of people sharing a network to a vector, and exclude anyone who lives with them
+    # Assign the indices of people sharing a network to a vector, and exclude 
+    # anyone who lives with them
     social_indices <- which(network_probs > probs)[-house_network[[i]]]
     
     # append the indices of people in person i's network to the list
     social_network[[i]] <- c(social_network[[i]], social_indices)
     
     # append person i to the network of each person in person i's network
-    social_network[social_indices] <- lapply(social_network[social_indices], function(x) c(x, i))
+    social_network[social_indices] <- lapply(social_network[social_indices], 
+                                             function(x) c(x, i))
   }
   
   return(social_network)
 }
 
 
-nseir <- function(beta,h,alink,alpha=c(.1,.01,.01),delta=.2,gamma=.4,nc=15, nt = 100,pinf = .005) {
-  #' This function implements and simulates the SEIR model described in the introduction to this document for a given set of parameter
-  #' values. It calculates the amount of people that are in the susceptible, exposed, infected, and recovered classes each day based 
-  #' on households, a personal social network, and a random interaction network (where households and the personal networks are disjoint). 
-  #' @param beta A list of uniform random deviates
-  #' @param h A vector indexing who shares houses
-  #' @param alink A list of vectors, with the vector at position i being the social network of person i.
-  #' @param alpha A vector of parameters for alpha_h, alpha_c, and alpha_r (as described above) respectively.
-  #' @param delta The probability that an infected individual transfers from the infected class to the recovered class each day.
-  #' @param gamma The probability that an exposed individual transfers from the exposed class to the infected class each day.
-  #' @param n_c A value of the average number of personal connections per person.
-  #' @param nt The number of days to simulate the model over.
-  #' @param pinf The proportion of the population infected at the start of the simulation.
+nseir <- function(beta,h,alink,alpha=c(.1,.01,.01),delta=.2,gamma=.4,nc=15, 
+                  nt = 100,pinf = .005) {
+  #' This function implements and simulates the SEIR model described in the 
+  #' introduction to this document for a given set of parameter values. It 
+  #' calculates the amount of people that are in the susceptible, exposed, 
+  #' infected, and recovered classes each day based on households, a personal 
+  #' social network, and a random interaction network (where households and the 
+  #' personal networks are disjoint). 
+  #' @param beta  A list of n uniform random deviates
+  #' @param h     An n vector indexing who shares houses
+  #' @param alink An n list of vectors, with the vector at position i being the 
+  #'              social network of person i.
+  #' @param alpha A vector of parameters for alpha_h, alpha_c, and alpha_r 
+  #'              (as described above) respectively.
+  #' @param delta The probability that an infected individual transfers from the 
+  #'              infected class to the recovered class each day.
+  #' @param gamma The probability that an exposed individual transfers from the 
+  #'              exposed class to the infected class each day.
+  #' @param n_c   A value of the average number of personal connections per 
+  #'              person.
+  #' @param nt    The number of days to simulate the model over.
+  #' @param pinf  The proportion of the population infected at the start of the 
+  #'              simulation.
   #' 
-  #' @return A list of vectors, where these vectors contain the number of people in the susceptible class, exposed class, infected class, 
-  #' recovered class, and the day these correspond to respectively.
+  #' @return A list of vectors, where these vectors contain the number of people 
+  #'         in the susceptible class, exposed class, infected class, recovered 
+  #'         class, and the day these correspond to respectively.
   
   # Sets the number of people in the model
   n <- length(h)
@@ -120,37 +144,46 @@ nseir <- function(beta,h,alink,alpha=c(.1,.01,.01),delta=.2,gamma=.4,nc=15, nt =
     # Initialise the vector to store people infected each day
     any_inf_vec <- rep(0,n)
     
-    # Iterate over each infected person to find out which susceptible people are infected by at least one infected person
+    # Iterate over each infected person to find out which susceptible people are 
+    # exposed by at least one infected person
     for (i in which(x==2)){
 
-      # Create a new vector where the jth entry will be the probability that person i infects person j by household or by the personal network
+      # Create a new vector where the jth entry will be the probability that 
+      # person i infects person j by household or by the personal network
       personal_con <- rep(0,n)
       
-      # If person j is in the household network of person i, set the jth value in personal_con to alpha_h
+      # If person j is in the household network of person i, set the jth value 
+      # in personal_con to alpha_h
       personal_con[which(h==h[i])] <- alpha[1]
-      # If person j is in the household network of person i, set the jth value in personal_con to alpha_c
+      # If person j is in the household network of person i, set the jth value 
+      # in personal_con to alpha_c
       personal_con[alink[[i]]] <- alpha[2]
       
-      # Find the probabilities of person i infecting each other person in the random network.
+      # Find the probabilities of person i infecting each other person in the 
+      # random network.
       rnet_con <- k * beta[i]
       
-      # Find which people are conncected to person i by household or personal network
+      # Find which people are connected to person i by household or personal 
+      # network
       con_ii <- which(personal_con != 0)
       
-      # Make a vector of uniform random deviates where only indices that correspond to contacts are sampled
-      unif1 <- rep(1,n)
-      unif1[con_ii] <- runif(length(con_ii))
+      unif1 <- rep(1,n) # To compare with random mixing
+      unif1[con_ii] <- runif(length(con_ii)) # To compare with house & social networks
       
-      # Simulate the probability that each person was infected by person i by household or personal network
+      # Simulate the probability that each person was infected by person i by 
+      # household or personal network
       inf_vec1 <- personal_con >= unif1
-      # Simulate the probability that each person was infected by person i by random network
+      # Simulate the probability that each person was infected by person i by 
+      # random network
       inf_vec2 <- rnet_con >= runif(n)
       
-      # Stores whether each person was infected by any infected person in the model on day t
+      # Stores whether each person was infected by any infected person in the 
+      # model on day t
       any_inf_vec <- inf_vec1 | inf_vec2 | any_inf_vec
     }
     
-    # Create uniform random deviates
+    # Create uniform random deviates to simulate moving to infected and recovered
+    # states
     u <- runif(n)
     
     # Infected people transition to recovered with class probability delta            
@@ -167,7 +200,8 @@ nseir <- function(beta,h,alink,alpha=c(.1,.01,.01),delta=.2,gamma=.4,nc=15, nt =
     t_vec[t] <- t
   }  
   
-  # Return the list of susceptible, exposed, infected, and recovered people on each day and the vector of day indices
+  # Return the list of susceptible, exposed, infected, and recovered people on 
+  # each day and the vector of day indices
   return(list(S=S,E=E,I=I,R=R,t=t_vec))
 }
 
@@ -217,7 +251,8 @@ plot_seir = function(seir_results_list, title = 'SEIR Model Results') {
 
         legend("topright",
               inset = c(0, 0),
-              legend = c("S (Susceptible)", "E (Exposed)", "I (Infectious)", "R (Recovered)"),
+              legend = c("S (Susceptible)", "E (Exposed)", "I (Infectious)", 
+                         "R (Recovered)"),
               col = c("black", "blue", "red", "green"),
               lty = 1,
               xpd = NA,
@@ -235,7 +270,8 @@ plot_seir = function(seir_results_list, title = 'SEIR Model Results') {
 
 system.time({
 # Run models
-# Specify the number of people in the simulation and the maximum number of people in a given household
+# Specify the number of people in the simulation and the maximum number of 
+# people in a given household
 n = 10000; hmax = 5
 
 # Simulate beta values as standard uniform random deviates
@@ -260,7 +296,8 @@ results_list <- list(
     # Run model with constant sociability parameters for each person
     ,nseir(beta=rep(mean(beta),n), h1, alink)
     
-    # Run model with constant sociability parameters for each person and only consider random mixing
+    # Run model with constant sociability parameters for each person and only 
+    # consider random mixing
     ,nseir(beta=rep(mean(beta),n), h1, alink, alpha=c(0, 0, 0.04))
 
 )
